971308359ee34bf38a98e43272f4ac0648bdb18e
var-bin <var-bin@users.noreply.github.com>
2017-03-05 20:15:33 +0200

    Work with gulp.dest

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,23 @@ const gulp = require("gulp");
 
 gulp.task("default", () => {
   return gulp.src("source/**/*.*")
-    .pipe(gulp.dest("dest"));
+    .on("data", (file) => {
+      console.log({
+        contents: file.contents,
+        path: file.path,
+        cwd: file.cwd,
+        base: file.base,
+        // path component helpers
+        relative: file.relative,
+        dirname: file.dirname,
+        basename: file.basename,
+        stem: file.stem,
+        extname: file.extname
+      });
+    })
+    .pipe(gulp.dest( (file) => {
+      return file.extname == ".js" ? "js" :
+             file.extname == ".css" ? "css" :
+             "dest";
+    }));
 });
