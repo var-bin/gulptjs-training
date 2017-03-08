7a8643e865d714f4a55329e94e66abde1a35452e
var-bin <var-bin@users.noreply.github.com>
2017-03-08 19:37:01 +0200

    Work with sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,28 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
 const debug = require("gulp-debug");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpIf = require("gulp-if");
+
+const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", () => {
   return gulp.src("source/styles/**/*.scss")
     .pipe(debug({
       title: "gulp.src"
     }))
+    .pipe(sourcemaps.init())
+    .pipe(debug({
+      title: "sourcemaps init"
+    }))
     .pipe(sass()
       .on("error", sass.logError))
     .pipe(debug({
       title: "sass"
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(debug({
+      title: "sourcemaps write"
     }))
     .pipe(concat("all.css"))
     .pipe(debug({
