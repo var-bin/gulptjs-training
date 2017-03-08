1d632e7f2f9c527523f02004dc87ba68facd7a99
var-bin <var-bin@users.noreply.github.com>
2017-03-08 19:53:51 +0200

    Edit styles task. Add clean, build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,26 @@ const concat = require("gulp-concat");
 const debug = require("gulp-debug");
 const sourcemaps = require("gulp-sourcemaps");
 const gulpIf = require("gulp-if");
+const del = require("del");
+
+const path = require("path");
 
 const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
+const DEST = path.join(__dirname, "dest");
+
 gulp.task("styles", () => {
   return gulp.src("source/styles/**/*.scss")
-    .pipe(debug({
-      title: "gulp.src"
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(debug({
-      title: "sourcemaps init"
-    }))
+    .pipe(gulpIf(IS_DEVELOPMENT, sourcemaps.init()))
     .pipe(sass()
       .on("error", sass.logError))
-    .pipe(debug({
-      title: "sass"
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(debug({
-      title: "sourcemaps write"
-    }))
-    .pipe(concat("all.css"))
-    .pipe(debug({
-      title: "concat"
-    }))
-    .pipe(gulp.dest("dest"));
+    .pipe(gulpIf(IS_DEVELOPMENT, sourcemaps.write()))
+    .pipe(gulpIf(IS_DEVELOPMENT, concat("all.css")))
+    .pipe(gulp.dest(DEST));
 });
+
+gulp.task("clean", () => {
+  return del(DEST);
+});
+
+gulp.task("build", gulp.series("clean", "styles"));
